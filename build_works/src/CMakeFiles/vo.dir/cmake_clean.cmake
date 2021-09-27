a1b80297dd19c2936ed5f81e40a66f4e63a0b400
Luis <luis@ip-172-31-68-7.ec2.internal>
2021-09-27 14:23:18 +0000

    Updates

---
+++ build_works/src/CMakeFiles/vo.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vo.dir/vo/frame.cpp.o"
+  "CMakeFiles/vo.dir/vo/vo.cpp.o"
+  "CMakeFiles/vo.dir/vo/vo_addFrame.cpp.o"
+  "CMakeFiles/vo.dir/vo/vo_io.cpp.o"
+  "CMakeFiles/vo.dir/vo/map.cpp.o"
+  "CMakeFiles/vo.dir/vo/mappoint.cpp.o"
+  "CMakeFiles/vo.dir/vo/vo_commons.cpp.o"
+  "../../lib/libvo.pdb"
+  "../../lib/libvo.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build_works/test/CMakeFiles/test_epipolor_geometry.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_epipolor_geometry.dir/test_epipolor_geometry.cpp.o"
+  "../../bin/test_epipolor_geometry.pdb"
+  "../../bin/test_epipolor_geometry"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_epipolor_geometry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
